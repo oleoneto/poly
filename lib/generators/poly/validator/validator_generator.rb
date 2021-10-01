991076ac7bcc209e6f317bd0d01d61b1116338f1
Leo <oleoneto@gmail.com>
2021-10-01 10:56:49 -0400

    Add generators

---
+++ lib/generators/poly/validator/validator_generator.rb
@@ -0,0 +1,11 @@
+module Poly
+  module Generators
+    class ValidatorGenerator < Rails::Generators::NamedBase
+      source_root File.expand_path('templates', __dir__)
+
+      def create_concern
+        template("validator.rb", File.join("app/validators/#{class_name.underscore}_validator.rb"))
+      end
+    end
+  end
+end
