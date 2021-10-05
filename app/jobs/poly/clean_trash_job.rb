a6740963001142a149a206e4bcf307a9dbeb12ac
Leo <oleoneto@gmail.com>
2021-10-05 03:49:33 -0400

    Namespace CleanTrashJob

--- app/jobs/poly/clean_trash_job.rb
+++ app/jobs/poly/clean_trash_job.rb
@@ -1,9 +1,11 @@
-class CleanTrashJob < Poly::ApplicationJob
-  queue_as :default
+module Poly
+  class CleanTrashJob < ApplicationJob
+    queue_as :default
 
-  # Delete the parent record and then remove the trash item
-  def perform(trash)
-    trash.trashable.destroy
-    trash.destroy
+    # Delete the parent record and then remove the trash item
+    def perform(trash)
+      trash.trashable.destroy
+      trash.destroy
+    end
   end
 end
