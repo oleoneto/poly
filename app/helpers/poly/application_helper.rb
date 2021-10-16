49ada47df767b0080aec7c93671cd319d3832155
Leo Neto <oleoneto@gmail.com>
2021-10-16 04:20:26 -0400

    Update article and comment templates

--- app/helpers/poly/application_helper.rb
+++ app/helpers/poly/application_helper.rb
@@ -1,5 +1,14 @@
 module Poly
   module ApplicationHelper
     include Pagy::Frontend
+
+    def sign_in_path
+      "#"
+    end
+
+    def is_logged_in?
+      current_user if defined?(current_user)
+      false
+    end
   end
 end
