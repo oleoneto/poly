8fe4391b4dc2f4774395072f0ac3c08702f946ca
Leo Neto <oleoneto@gmail.com>
2021-10-23 02:29:40 -0400

    Update configure to fix Bundler error

--- sandbox/config/application.rb
+++ sandbox/config/application.rb
@@ -7,7 +7,7 @@ require "rails/all"
 Bundler.require(*Rails.groups)
 require "poly"
 
-module Dummy
+module Sandbox
   class Application < Rails::Application
     config.load_defaults Rails::VERSION::STRING.to_f
 
